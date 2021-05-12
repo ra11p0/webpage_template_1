59dd4e56eebba138565257a3e6ace9b08dd594f2
ra11p0 <p.lenczewski@icloud.com>
2021-05-12 17:18:36 +0200

    finished

--- js.js
+++ js.js
@@ -1,3 +1,76 @@
+function init(){
+    document.getElementById("sendMailButton").addEventListener("click", sendMail);
+
+    document.getElementById("loginButton").addEventListener("click", login);
+    
+    document.getElementById("registerButton").addEventListener("click", register);
+}
+
+function sendMail(){
+    if (document.getElementById('sendMailEMail').value != document.getElementById('sendMailEMailConfirm').value){
+        alert("Adresy e-mail nie są identyczne!");
+        document.getElementById('sendMailEMail').style.background = "rgba(255, 0, 0, 0.2)";
+        document.getElementById('sendMailEMailConfirm').style.background = "rgba(255, 0, 0, 0.2)";
+    }
+    else if (!document.getElementById('sendMailEMail').value.includes('@') || !document.getElementById('sendMailEMail').value.includes('.')){
+        alert("Adres e-mail nie jest poprawny!");
+        document.getElementById('sendMailEMail').style.background = "rgba(255, 0, 0, 0.2)";
+    }
+    else{
+        document.getElementById('sendMailEMail').style.background = "rgba(0, 0, 0, 0.2)";
+        document.getElementById('sendMailEMailConfirm').style.background = "rgba(0, 0, 0, 0.2)";
+        alert("Wysłano!");
+    }
+}
+
+function login(){
+    alert("Zalogowano!");
+}
+
+function register(){
+    let isAllOkay = 1;
+    if (document.getElementById('registerEmail').value != document.getElementById('registerEmailConfirm').value){
+        alert("Adresy e-mail nie są identyczne!");
+        isAllOkay = 0;
+        document.getElementById('registerEmail').style.background = "rgba(255, 0, 0, 0.2)";
+        document.getElementById('registerEmailConfirm').style.background = "rgba(255, 0, 0, 0.2)";
+    }
+    else if (!document.getElementById('registerEmail').value.includes('@') || !document.getElementById('registerEmail').value.includes('.')){
+        alert("Adres e-mail nie jest poprawny!");
+        isAllOkay = 0;
+        document.getElementById('registerEmail').style.background = "rgba(255, 0, 0, 0.2)";
+    }
+    else {
+        document.getElementById('registerEmail').style.background = "rgba(0, 0, 0, 0.2)";
+        document.getElementById('registerEmailConfirm').style.background = "rgba(0, 0, 0, 0.2)";
+    }
+    if (document.getElementById('registerPassWd').value != document.getElementById('registerPassWdConfirm').value){
+        alert("Hasła nie są identyczne!");
+        isAllOkay = 0;
+        document.getElementById('registerPassWd').style.background = "rgba(255, 0, 0, 0.2)";
+        document.getElementById('registerPassWdConfirm').style.background = "rgba(255, 0, 0, 0.2)";
+    }
+    else if (document.getElementById('registerPassWd').value.length < 8){
+        alert("Hasło powinno zawierać przynajmniej 8 znaków!");
+        isAllOkay = 0;
+        document.getElementById('registerPassWd').style.background = "rgba(255, 0, 0, 0.2)";
+    }
+    else{
+        document.getElementById('registerPassWd').style.background = "rgba(0, 0, 0, 0.2)";
+        document.getElementById('registerPassWdConfirm').style.background = "rgba(0, 0, 0, 0.2)";
+    }
+    if(!document.getElementById('registerCheckBox').checked)
+    {
+        alert("Musisz zaakceptować regulamin!");
+        isAllOkay = 0;
+    }
+    if (isAllOkay)
+    {
+        alert("Zarejestrowano!");
+    }
+    
+}
+
 window.onscroll = function(ev) {
     if ((window.innerHeight + window.scrollY) <= 2000) {
         document.getElementById('quote').innerHTML = 
@@ -12,7 +85,6 @@ window.onscroll = function(ev) {
     }
 };
 
-
 function hidePhotoRoll(){
     document.getElementById("photoRoll").style.visibility = "hidden";
 }
@@ -24,6 +96,7 @@ function showPhotoRoll(e){
     hideRegister();
     hideLogin();
 }
+
 function nextPhoto(){
     var currentImage = document.getElementById("photoField").src;
     var currentImageNumber = currentImage.substring(currentImage.length - 5, currentImage.length-4);
@@ -31,6 +104,7 @@ function nextPhoto(){
     currentImageNumber++;
     document.getElementById("photoField").src = "media/"+currentImageNumber+".jpg";
 }
+
 function prevPhoto(){
     var currentImage = document.getElementById("photoField").src;
     var currentImageNumber = currentImage.substring(currentImage.length - 5, currentImage.length-4);
@@ -38,17 +112,21 @@ function prevPhoto(){
     currentImageNumber--;
     document.getElementById("photoField").src = "media/"+currentImageNumber+".jpg";
 }
+
 function showLogin(){
     document.getElementById("logIn").style.visibility = "visible";
     hideRegister();
 }
+
 function hideLogin(){
     document.getElementById("logIn").style.visibility = "hidden";
 }
+
 function showRegister(){
     document.getElementById("signUp").style.visibility = "visible";
     hideLogin();
 }
+
 function hideRegister(){
     document.getElementById("signUp").style.visibility = "hidden";
 }
